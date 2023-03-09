6280e051967330afb831bda97098bb87d595b053
Danilo Henrique <danilomhenrique311@gmail.com>
2023-03-09 17:17:15 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 //		Aqui foi criada uma injeção de dependecia onde para
 //		ser criado um vendedor, o programador necessita de 
 //		utilizar a classe DaoFactory e não instanciando direto
@@ -49,6 +51,14 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Updated complete!");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deleted completed");
+		
+		sc.close();
 	}
 
 }
