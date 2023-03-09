1accaefee39f5546ea8a408feceb457612ce3566
Danilo Henrique <danilomhenrique311@gmail.com>
2023-03-09 16:22:52 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,25 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
 
+//		Aqui foi criada uma injeção de dependecia onde para
+//		ser criado um vendedor, o programador necessita de 
+//		utilizar a classe DaoFactory e não instanciando direto
+//		pela classe SellerDao, pois há um encadeamento de 
+//		dependencia ja que a classe sellerDao
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public class DaoFactory {
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
 
+public class DaoFactory {
+	
+//	Aqui ocorreu uma injeção de independecia onde para ser criado um
+//	novo vendedor precisa da criação de conexão com o banco de dados
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
 }
