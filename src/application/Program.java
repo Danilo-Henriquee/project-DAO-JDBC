396c78bd8b30d99f96c1a74f34ba885fb2ba9dcb
Danilo Henrique <danilomhenrique311@gmail.com>
2023-03-09 16:28:19 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,6 +18,7 @@ public class Program {
 //		dependencia ja que a classe sellerDao
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
